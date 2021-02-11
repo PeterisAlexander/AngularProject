874e388a179c045fa8a80e67a16686830bdafa59
Pierre DESGOUILLES <p.desgouilles@ymag.fr>
2021-02-11 18:33:11 +0100

    Creation du menu madera

--- Angular-Madera2/src/app/app-routing.module.ts
+++ Angular-Madera2/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardHomeComponent } from './business/dashboard/page/dashboard-home/dashboard-home.component';
+import { HomeComponent } from './business/home/home.component';
 import { LoginComponent } from './business/login/login.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent,
+  },
+  {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardHomeComponent,
   }
 ];
 

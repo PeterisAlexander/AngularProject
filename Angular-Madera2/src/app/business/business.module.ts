17653c4c1604449408f3c845b8da96716fdfabc8
PeterisAlexander <66838027+PeterisAlexander@users.noreply.github.com>
2021-02-11 12:04:02 +0100

    Feature/task/0031 header menu (#17)
    * Ajout des modules Lib et UI
    
    * Rename Dashboard file
    
    Co-authored-by: Pierre DESGOUILLES <p.desgouilles@ymag.fr>

--- Angular-Madera2/src/app/business/business.module.ts
+++ Angular-Madera2/src/app/business/business.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardHomeComponent } from './dashboard/page/dashboard-home/dashboard-home.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, DashboardComponent, HomeComponent],
+  declarations: [LoginComponent, DashboardHomeComponent, HomeComponent],
   imports: [
     CommonModule
   ]

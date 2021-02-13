a300154a207f61389df3b1720641b073abbe29f6
PeterisAlexander <pad7887@gmail.com>
2021-02-13 20:44:42 +0100

    Ajout menu et menuProfile et modif style

--- Angular-Madera2/src/app/business/business.module.ts
+++ Angular-Madera2/src/app/business/business.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardHomeComponent } from './dashboard/page/dashboard-home/dashboard-home.component';
+import { DashboardHeaderComponent } from './dashboard/page/dashboard-header/dashboard-header.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, DashboardHomeComponent, HomeComponent],
+  declarations: [LoginComponent, DashboardHomeComponent, HomeComponent, DashboardHeaderComponent],
   imports: [
     CommonModule
   ]

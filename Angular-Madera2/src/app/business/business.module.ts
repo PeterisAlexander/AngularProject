4d375f27099e86c0ac9b9079b0b2bd5e985ef00f
PeterisAlexander <66838027+PeterisAlexander@users.noreply.github.com>
2021-02-14 19:42:06 +0100

    Feature/us/0030 creation menu cards (#18)
    * Feature/task/0031 header menu (#17)
    
    * Ajout des modules Lib et UI
    
    * Rename Dashboard file
    
    Co-authored-by: Pierre DESGOUILLES <p.desgouilles@ymag.fr>
    
    * Creation du menu madera
    
    * Ajout menu et menuProfile et modif style
    
    * Ajout cards
    
    Co-authored-by: Pierre DESGOUILLES <p.desgouilles@ymag.fr>

--- Angular-Madera2/src/app/business/business.module.ts
+++ Angular-Madera2/src/app/business/business.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardHomeComponent } from './dashboard/page/dashboard-home/dashboard-home.component';
+import { DashboardHeaderComponent } from './dashboard/page/dashboard-header/dashboard-header.component';
+import { LibModule } from '../lib/lib.module';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, DashboardComponent, HomeComponent],
+  declarations: [CardComponent, LoginComponent, DashboardHomeComponent, HomeComponent, DashboardHeaderComponent],
   imports: [
+    LibModule,
     CommonModule
   ]
 })
---
+++ Angular-Madera2/src/app/business/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard-home.component.html',
+  styleUrls: ['./dashboard-home.component.css']
+})
+export class DashboardHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

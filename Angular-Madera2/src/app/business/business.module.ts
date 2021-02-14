7a38ffb7508c913f453308364a393d92d9a8f102
PeterisAlexander <pad7887@gmail.com>
2021-02-14 19:34:53 +0100

    Ajout cards

--- Angular-Madera2/src/app/business/business.module.ts
+++ Angular-Madera2/src/app/business/business.module.ts
@@ -4,12 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardHomeComponent } from './dashboard/page/dashboard-home/dashboard-home.component';
 import { DashboardHeaderComponent } from './dashboard/page/dashboard-header/dashboard-header.component';
+import { LibModule } from '../lib/lib.module';
+import { CardComponent } from './card/card.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, DashboardHomeComponent, HomeComponent, DashboardHeaderComponent],
+  declarations: [CardComponent, LoginComponent, DashboardHomeComponent, HomeComponent, DashboardHeaderComponent],
   imports: [
+    LibModule,
     CommonModule
   ]
 })
